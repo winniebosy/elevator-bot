3a38f00727f29b7709601496cb23111fe716083b
Winnie Bosibori <winniebosy@gmail.com>
2022-04-01 05:47:20 +0300

    instructions

---
+++ elevator.py
@@ -0,0 +1,4 @@
+#It prints messages for the user, with a short pause between each message
+#It asks the user to make a choice (and handles invalid input without crashing)
+#Depending on what the user chooses, something different happens
+#It keeps running forever, letting you pick different floors over and over
